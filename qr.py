36a8040cf4a9d8002d180a07eb5a0d705f31c737
Tonmoy Chandra Mudi <mudi242-35-507@diu.edu.bd>
2025-12-14 00:17:27 +0600

    first commit

---
+++ qr.py
@@ -0,0 +1,11 @@
+import qrcode
+url = input("Enter the URL: ").strip()
+file_path = "C:\\Users\\TCM\\Downloads\\qrcode.png"
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR Code generated successfully.")
